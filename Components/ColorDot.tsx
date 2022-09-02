5146f0f38f7310804d7e439ba7fc6c89d85ade61
Paulo Victor SS <paulovss@bb.com.br>
2022-09-02 18:53:49 -0300

    Finished

--- Components/ColorDot.tsx
+++ Components/ColorDot.tsx
@@ -5,6 +5,8 @@ import {
   TouchableOpacity,
   TextInput,
   TouchableHighlight,
+  Alert,
+  StyleSheet,
 } from 'react-native';
 import React, {useState} from 'react';
 
@@ -15,59 +17,74 @@ export default function ColorDot() {
   function handleChange(change: string) {
     let check = /^#([0-9a-f]{3}){1,2}$/i;
     if (check.test('#' + change)) {
-      setHex(prev => '#'+change);
+      setHex(prev => '#' + change);
       console.log('foi', hex);
+    } else {
+      if (change.length === 6) {
+        Alert.alert('HEX Inválido', 'Digite outro');
+      }
     }
   }
   return (
-    <View>
-      <TextInput
-        value={currentText}
-        onChangeText={current_text => setCurrentText(current_text)}
-      />
-      <TouchableOpacity
-        style={{width: 10, height: 10, backgroundColor: 'blue'}}
-        onPress={() => handleChange(currentText)}
-      />
+    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{flexDirection: 'row', alignItems: 'center', margin:10}}>
+        <Text style={styles.text}>#</Text>
+        <TextInput
+          style={styles.input}
+          value={currentText}
+          maxLength={6}
+          placeholder="Digite o código HEX:"
+          onChangeText={current_text => {
+            setCurrentText(current_text);
+            handleChange(current_text);
+          }}
+        />
+      </View>
       <View
         style={{
           backgroundColor: hex,
           borderRadius: 50,
           width: 60,
           height: 60,
+          margin: 10,
+          borderWidth: 1,
         }}
       />
     </View>
   );
 }
 
-const styles = (boo: boolean) =>
-  StyleSheet.create({
-    container: {
-      width: 200,
-      backgroundColor: 'white',
-      marginTop: boo ? 10 : 5,
-      marginBottom: boo ? 10 : 5,
-      marginHorizontal: 10,
-      borderRadius: 5,
-      elevation: 5,
-      shadowColor: '#52006A',
-      display: 'flex',
-      alignItems: 'center',
-      padding: 0,
-    },
-    subContainer: {
-      borderColor: '#507DBC',
-      borderBottomWidth: 1,
-      width: '100%',
-      margin: 0,
-      borderTopRightRadius: 5,
-      borderTopLeftRadius: 5,
-      backgroundColor: '#BBD1EA',
-    },
-    text: {
-      color: '#083D77',
-      fontFamily: 'sans-serif-condensed',
-      textAlign: 'center',
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    width: 200,
+    backgroundColor: 'white',
+    marginHorizontal: 10,
+    borderRadius: 5,
+    elevation: 5,
+    shadowColor: '#52006A',
+    display: 'flex',
+    alignItems: 'center',
+    padding: 0,
+  },
+  subContainer: {
+    borderColor: '#507DBC',
+    borderBottomWidth: 1,
+    width: '100%',
+    margin: 0,
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5,
+    backgroundColor: '#BBD1EA',
+  },
+  text: {
+    color: '#083D77',
+    fontFamily: 'sans-serif-condensed',
+    textAlign: 'center',
+    fontSize: 25,
+    margin: 5,
+  },
+  input: {
+    borderWidth: 1,
+    width: '37%',
+    height: 40,
+  },
+});
